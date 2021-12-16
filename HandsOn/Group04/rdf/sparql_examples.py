7f88bff2a684f36403439f81301006ec1674f57b
Pablo <pablete.arino@gmail.com>
2021-12-16 17:34:34 +0100

    update sparql_examples.py

--- HandsOn/Group04/rdf/sparql_examples.py
+++ HandsOn/Group04/rdf/sparql_examples.py
@@ -4,82 +4,73 @@ from rdflib.plugins.sparql import prepareQuery
 
 def main():
     g = Graph()
-    AQP1 = Namespace("http://www.airqualitypredictor.org/data/")
-    AQP2 = Namespace("http://www.airqualitypredictor.org/ontology#")
+    AQP_DATA = Namespace("http://www.airqualitypredictor.org/data/")
+    AQP_CLASSES = Namespace("http://www.airqualitypredictor.org/ontology/")
+    AQP_PROPERTIES = Namespace("http://www.airqualitypredictor.org/ontology#")
 
     g.parse('../ontology/ontology.owl', format='ttl')
-    g.parse('output-with-links-utf8.ttl', format='ttl')
-
-    # for s, p, o in g.triples((None, None, None)):
-    #     print(s, p, o)
-
-    # Query 0
-    query = """
-               SELECT ?temp
-                WHERE{
-                    ?temp a aqp:Temperature .
-                }
-               """
-    q = prepareQuery(query, initNs={"aqp": AQP1, "aqp2": AQP2})
-    result = g.query(q)
-    print("Query 0:")
-    for row in result:
-        print(row)
+    g.parse('output-diciembre-utf8.ttl', format='ttl')
 
     # # Query 1
     # query = """
-    #         SELECT ?ctrlstation ?type
+    #         SELECT ?control_station
     #         WHERE{
-    #             ?ctrlstation a ?type .
+    #             ?control_station a aqp_classes:Suburban .
     #         }
+    #         LIMIT 10
     #         """
-    # q = prepareQuery(query, initNs={"aqp": AQP})
+    # q = prepareQuery(query, initNs={"aqp_data": AQP_DATA, "aqp_classes": AQP_CLASSES, "aqp_properties": AQP_PROPERTIES})
     # result = g.query(q)
     # print("Query 1:")
     # for row in result:
     #     print(row)
 
-    # Query 2
-    query = """
-            SELECT ?temp ?fecha
-            WHERE{
-                ?temp a aqp:Temperature .
-                ?year a aqp:atYear .
-                ?fecha a aqp:atDate .
-                FILTER(?year = 2020)
-            }
-            """
-    q = prepareQuery(query, initNs={"aqp": AQP1, "aqp2": AQP2})
-    result = g.query(q)
-    print("\nQuery 2:")
-    for row in result:
-        print(row)
+    # # Query 2
+    # query = """
+    #         SELECT ?sub ?max_value ?day
+    #         WHERE{
+    #             ?sub a aqp_classes:PM10 .
+    #             ?sub aqp_properties:hasMaxValue ?max_value .
+    #             ?sub aqp_properties:atDay ?day .
+    #             FILTER(?day = 1.0)
+    #         }
+    #         ORDER BY DESC(?max_value) LIMIT 10
+    #         """
+    # q = prepareQuery(query, initNs={"aqp_data": AQP_DATA, "aqp_classes": AQP_CLASSES, "aqp_properties": AQP_PROPERTIES})
+    # result = g.query(q)
+    # print("\nQuery 2:")
+    # print("Top 10 stations with the highest PM10 concentration in the first day of the year")
+    # # print("Max value of temperature in December 2020:")
+    # for row in result:
+    #     print(row)
 
-    # Query 3
-    query = """
-            SELECT ?maxtemp ?fecha
-            WHERE{
-                ?sub a aqp:Suburban .
-                ?temp a aqp:temperature .
-                ?fecha a aqp:atDate .
-                ?temp aqp:hasMaxValue ?maxtemp
-                FILTER(?maxtemp > 15)
-            }
-            """
-    q = prepareQuery(query, initNs={"aqp": AQP1, "aqp2": AQP2})
-    result = g.query(q)
-    print("\nQuery 3:")
-    for row in result:
-        print(row)
+    # # Query 3
+    # control_station = 58  # ?max_N0_2 ?max_0_3 ?max_PM2.5
+    # query = f"""
+    #         SELECT ?sub1 ?max_PM10
+    #         WHERE {"{"}
+    #             ?sub1 a aqp_classes:PM10 .
+    #             ?sub1 aqp_properties:hasMaxValue ?max_PM10 .
+    #             ?sub1 apq_properties:controlStation {control_station} .
+    #         {"}"}
+    #         """
+    #
+    # print(query)
+    # q = prepareQuery(query, initNs={"aqp_data": AQP_DATA, "aqp_classes": AQP_CLASSES, "aqp_properties": AQP_PROPERTIES})
+    # result = g.query(q)
+    # print("\nQuery 3:")
+    # for row in result:
+    #     print(row)
 
-    # Query 4
+    # Query 4: see all properties of a measurement
     query = """
-            SELECT ?sub
+            SELECT DISTINCT ?prop
             WHERE{
-                ?sub a aqp:Suburban .
+                ?sub a aqp_classes:PM10 .
+                ?sub ?prop ?value .
             }
             """
-    q = prepareQuery(query, initNs={"aqp": AQP1, "aqp2": AQP2})
+    q = prepareQuery(query, initNs={"aqp_data": AQP_DATA, "aqp_classes": AQP_CLASSES, "aqp_properties": AQP_PROPERTIES})
     result = g.query(q)
     print("\nQuery 4:")
     for row in result:
